e2fbea2286d1d1ac3380bf6ed8c5d319cca996a6
DTipan <ajas.bakran@gmail.com>
2019-01-13 02:24:49 +0530

    Completed sample react Movie display app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import { fakemovies } from "./fakeGenreService";
 
 class App extends Component {
+  state = {
+    movies: fakemovies
+  };
+
+  handleDelete = movie => {
+    const movies = this.state.movies.filter(m => m._id !== movie._id);
+    this.setState({ movies });
+  };
+  GetMoviesData() {
+    if (this.state.movies.length === 0)
+      return (
+        <p style={{ textAlign: "center" }}>Opps..No movies in the database</p>
+      );
+    return this.generateMoviesData();
+  }
+
+  generateMoviesData() {
+    return (
+      <main className="container">
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Title</th>
+              <th scope="col">Genre</th>
+              <th scope="col" />
+            </tr>
+          </thead>
+
+          {this.state.movies.map(item => {
+            return (
+              <tbody key={item._id}>
+                <tr>
+                  <th scope="row">{item._id}</th>
+                  <td>{item.title}</td>
+                  <td>{item.genre}</td>
+                  <td>
+                    <button
+                      onClick={() => this.handleDelete(item)}
+                      className="btn btn-danger"
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              </tbody>
+            );
+          })}
+        </table>
+      </main>
+    );
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h3 style={{ textAlign: "center" }}>
+          Welcome to Movie Reviews Website
+        </h3>
+        {this.GetMoviesData()}
       </div>
     );
   }
---
+++ src/fakeGenreService.js
@@ -0,0 +1,22 @@
+export const fakemovies = [
+  {
+    _id: 1,
+    title: "Terminator",
+    genre: "Action"
+  },
+  {
+    _id: 2,
+    title: "X-men",
+    genre: "Action"
+  },
+  {
+    _id: 3,
+    title: "Wolverin",
+    genre: "Action"
+  },
+  {
+    _id: 4,
+    title: "Die Hard",
+    genre: "Action"
+  }
+];
